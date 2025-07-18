8e26b2629006e393a0578867a648326ecdf6a5d5
sgr-0007 <sagarhs0007@gmail.com>
2025-07-18 10:10:28 +0100

    feat: seed roles for Admin and User in AspNetRoles table

---
+++ Core/Stocks.API/Migrations/20250718090303_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Stocks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "17942135-11d3-43da-9b05-9f1dbf53644e", null, "User", "USER" },
+                    { "fbddc08b-b308-4a4c-8f84-55c27e18dd9f", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "17942135-11d3-43da-9b05-9f1dbf53644e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fbddc08b-b308-4a4c-8f84-55c27e18dd9f");
+        }
+    }
+}
